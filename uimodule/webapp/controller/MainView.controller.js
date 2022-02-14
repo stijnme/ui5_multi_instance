30717a3ae5515d83282f096fb08fe1e84d8734ee
Stijn Mertens <stijn.mertens@expertum.net>
2022-02-14 21:16:49 +0100

    Added tabcontainers

--- uimodule/webapp/controller/MainView.controller.js
+++ uimodule/webapp/controller/MainView.controller.js
@@ -7,7 +7,31 @@ sap.ui.define(
         "use strict";
 
         return Controller.extend("net.expertum.MultiInstance.controller.MainView", {
-            onInit: function () {},
+            onInit: function () {
+                let oBPModel = new sap.ui.model.json.JSONModel({
+                    "BPCollection": [
+                        {
+                            "BPId": "123456",
+                            "FirstName": "Jehona",
+                            "LastName": "Matheson",
+                            "Birthday": "1886-12-13"
+                        },
+                        {
+                            "BPId": "123457",
+                            "FirstName": "Aarti",
+                            "LastName": "Simonsen",
+                            "Birthday": "1928-03-21"
+                        },
+                        {
+                            "BPId": "123458",
+                            "FirstName": "Judoc",
+                            "LastName": "Solberg",
+                            "Birthday": "1939-05-26"
+                        }
+                    ]});
+
+                this.getView().setModel(oBPModel);
+            }
         });
     }
 );
